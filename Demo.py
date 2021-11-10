80a93b932236d52470102188ea7e1a334df1eb70
Suhas Veda Prabhakar <suhasvp44@gmail.com>
2021-11-10 19:46:30 +0100

    add Demo.py

---
+++ Demo.py
@@ -0,0 +1,10 @@
+Python 3.9.4 (tags/v3.9.4:1f2e308, Apr  6 2021, 13:40:21) [MSC v.1928 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> Print ("Hello world")
+Traceback (most recent call last):
+  File "<pyshell#0>", line 1, in <module>
+    Print ("Hello world")
+NameError: name 'Print' is not defined
+>>> print("hello world")
+hello world
+>>> 
